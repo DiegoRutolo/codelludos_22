0f5492a6d6e460b5120e2777c0bcdcae0a371db2
Pedro <pedro.saco.vilas26@gmail.com>
2022-02-24 19:27:34 +0100

    Clase Trabajador/Proyecto

---
+++ Proyecto.java
@@ -0,0 +1,12 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+public class Proyecto 
+{
+    public String Nombre;
+    public Integer DiasNecesarios;
+    public Integer Puntuacion;
+    public Integer DiaLimite;
+    public ArrayList<Skill> Roles;
+}
---
+++ Trabajador.java
@@ -0,0 +1,9 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+public class Trabajador 
+{
+    public String Nombre;
+    public ArrayList<Skill> Skills;
+}
