8c9d5d07b96a31b5a82b9ddf19236bc82519a058
AdrianRodriguezVilas <adrianmailpublic@gmail.com>
2022-02-24 19:51:09 +0100

    Update OutputPrinter.java and Result.java

--- OutputPrinter.java
+++ OutputPrinter.java
@@ -1,8 +1,47 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class OutputPrinter {
-    
-    static public void printResult(ArrayList<Projec) {
 
+    public File outputFile;
+    FileWriter myWriter;
+
+    public void initialize() {
+        try {
+//            outputFile = new File("C:\\demo\\demofile.txt");
+//            outputStream = new FileOutputStream(outputFile);
+
+            outputFile = new File("filename.txt");
+            if (outputFile.createNewFile()) {
+                System.out.println("File created: " + outputFile.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            myWriter = new FileWriter("filename.txt");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
+
+
+    public void printResult(ArrayList<Result> resultados) {
+        for (Result resultado : resultados) {
+            try {
+                myWriter.write("Files in Java might be tricky, but it is fun enough!");
+                System.out.println("Successfully wrote to the file.");
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+
+        myWriter.close();
+    }
+
+
 }
