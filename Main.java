428e780fe3c6608e3893445e445111f7504aff2e
DiegoRutolo <diegorutolo@gmail.com>
2022-02-24 21:31:40 +0100

    getTrabajadoresBySill

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -17,13 +18,36 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 
-		Lector l = new Lector(CASO_F, trabajadores, proyectos);
-		trabajadores.forEach((n, t) -> System.out.println(t));
+		Lector l = new Lector(CASO_A, trabajadores, proyectos);
+
+		Skill s1 = new Skill("Python", 3);
+		// Skill s2 = new Skill("Haskell", 3);
+		// Skill s3 = new Skill("HTML-py", 1);
+
+		for (Entry<String, Trabajador> e : Main.getTrabajadoresBySill(s1).entrySet()) {
+			System.out.println(e.getValue());
+		}
 	}
 
-	public static Map<String, Trabajador> getTrabajadoresBySill(Skill s) {
-		return trabajadores.entrySet().parallelStream()
-				.filter(t -> t.getValue().skills.contains(s))
-				.collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+	public static HashMap<String, Trabajador> getTrabajadoresBySill(Skill skill) {
+		HashMap<String, Trabajador> ts = new HashMap<>();
+
+		for (Entry<String, Trabajador> e : trabajadores.entrySet()) {
+			for (Skill s : e.getValue().skills) {
+				if (skill.equals(s))
+					ts.put(e.getKey(), e.getValue());
+			}
+			
+		}
+		
+		return ts;
+	}
+
+	public static boolean contiene(ArrayList<Skill> skills, Skill skill) {
+		for (Skill s : skills) {
+			if (skill.equals(s))
+				return true;
+		}
+		return false;
 	}
 }
