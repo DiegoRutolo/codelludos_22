ca271063fd727b722a80e2dfd0f77f42fa1ad2cc
Iago Loureiro <iago.loureiro@enxine.com>
2022-02-24 19:53:24 +0100

    Hacer Estaticos

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
 public class Main {
-    ArrayList<Proyecto> proyectos=new ArrayList<Proyecto>();
-    ArrayList<Trabajador> trabajadores=new ArrayList<Trabajador>();
+    public static ArrayList<Proyecto> proyectos=new ArrayList<Proyecto>();
+    public static ArrayList<Trabajador> trabajadores=new ArrayList<Trabajador>();
     
     public static void main(String[] args) {
         System.out.println("Hola mundo");
