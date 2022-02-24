cfb4f399b9287f189329dc971d56d34ae47beead
DiegoRutolo <diegorutolo@gmail.com>
2022-02-24 20:24:19 +0100

    toString Trabajador

--- Trabajador.java
+++ Trabajador.java
@@ -2,11 +2,28 @@ import java.util.ArrayList;
 
 public class Trabajador 
 {
-    public String nombre;
-    public ArrayList<Skill> skills;
+	public String nombre;
+	public ArrayList<Skill> skills;
 
-    public Trabajador(String nombre)
-    {
-        this.nombre = nombre;
-    }
+	public Trabajador(String nombre)
+	{
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(nombre);
+		sb.append(" (");
+		for (Skill s : skills) {
+			sb.append(s.nombre);
+			sb.append(" ");
+			sb.append(s.nivel);
+			sb.append(", ");
+		}
+		sb.append(")");
+
+		return sb.toString();
+	}
 }
