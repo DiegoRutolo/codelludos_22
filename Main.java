0eb5ccc7bb61ceb1c60a419d094f6e802b76b991
Iago Loureiro <iago.loureiro@enxine.com>
2022-02-24 19:23:42 +0100

    Skill created

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import javax.sound.sampled.SourceDataLine;
+
 public class Main {
     public static void main(String[] args) {
+        Skill skill=new Skill("C++", 5);
+        
         System.out.println("Hola mundo");
+        System.out.println("Skill: "+skill.name + ": "+skill.level);
     }
 }
