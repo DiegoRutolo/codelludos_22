a1453180536d3ca8d1dd6d86136cfa11dc4a6d90
Pedro <pedro.saco.vilas26@gmail.com>
2022-02-24 20:49:23 +0100

    Nuevo metodo ordenacion proyectos

--- Sorter.java
+++ Sorter.java
@@ -6,7 +6,14 @@ import java.util.Comparator;
 
 public class Sorter {
 
-    public ArrayList<Proyecto> sortProject(ArrayList<Proyecto> projects) {
+    public ArrayList<Proyecto> sortProject(ArrayList<Proyecto> projects) { //Para usar descomentar el primer compareTO de Proyecto.java
+        Collections.sort(projects);
+
+        return projects;
+    }
+
+    public ArrayList<Proyecto> sortProject2(ArrayList<Proyecto> projects)
+    {
         Collections.sort(projects);
 
         return projects;
